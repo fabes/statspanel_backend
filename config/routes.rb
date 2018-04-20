0ee33245a78715951b8768ab3d0e32b38dc5aeb9
Fabion Stephens <fabions@octobits.io>
2018-04-20 02:00:02 -0500

    Worked on updating devise routes to respond to user data from front for user registrations

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       devise_for :users, controllers: {
-        sessions: 'api/v1/users/sessions'
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations',
       }
-    end 
+    end
   end
 end
