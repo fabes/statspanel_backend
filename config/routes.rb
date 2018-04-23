697e7b3a8cc5d1a697ccd80695144552975da242
Fabion Stephens <fabions@octobits.io>
2018-04-23 17:18:09 -0500

    Add projects controller and resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
+      resources :projects
+
       devise_for :users, controllers: {
         sessions: 'api/v1/users/sessions',
         registrations: 'api/v1/users/registrations',
       }
+      
       get 'has-valid-token', to: 'auth#has_valid_token'
     end
   end
