c3dff9553c7ce191f2370289ae18da49fa102df0
Fabion Stephens <fabions@octobits.io>
2018-04-23 23:04:23 -0500

    check for valid token on each request and started project CRUD

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,10 +1,33 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :auth_request 
+
   def index 
     render json:{
-      data: {
-        'p1': 'test1',
-        'p2': 'test2'
-      }
+      data: active_user.projects.all.select(:id, :name, :code)
     }
+  end
+
+  def create 
+    project = Project.new(allowed_project_params)
+    project.code = SecureRandom.hex(3)
+    project.user = active_user
+    project.status = 1
+
+    if project.save 
+      render json: {
+        data: active_user.projects.all.select(:id, :name, :code)
+      }
+    else
+      head(:bad_request)
+    end
+    
+  end
+  
+  private 
+
+  def allowed_project_params 
+    params.permit(
+      :name,
+    )
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Helpers
-  helper_method :valid_token?
+  helper_method :valid_token?, :active_user
 
   private
   def valid_token? 
-    auth_token = request.headers['HTTP_AUTH_TOKEN']
-    return true if User.find_by(authentication_token: auth_token)
+    return true if active_user
     return false
+  end
+
+  def auth_request 
+    head(:unauthorized) if !valid_token?
+  end
+
+  def active_user 
+    auth_token = request.headers['HTTP_AUTH_TOKEN']
+    User.find_by(authentication_token: auth_token)
   end
 end
