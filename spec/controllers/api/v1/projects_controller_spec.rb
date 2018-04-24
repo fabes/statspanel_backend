2006342690618cedbd4fd5e3444bd3f6b6d71111
Fabion Stephens <fabions@octobits.io>
2018-04-24 02:57:29 -0500

    Continue writing tests for projects_controller

--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ProjectsController, type: :request do
-  describe "#index" do 
-    let!(:user){ create :user }
+  let!(:user){ create :user }
 
+  describe "#index" do 
     before do 
       create :project, user: user, code: SecureRandom.hex(3)
       create :project, user: user, name: 'Test 2 project', code: SecureRandom.hex(3)
@@ -16,5 +16,19 @@ RSpec.describe Api::V1::ProjectsController, type: :request do
       expect(response.headers["Content-Type"]).to eq('application/json; charset=utf-8') 
     end
 
+  end
+
+  describe "#create" do 
+    it "should return success" do 
+      post api_v1_projects_path, 
+        params: { 
+          name: 'Test project 1', 
+          user: user, 
+          code: SecureRandom.hex(3) 
+        },
+        headers: { 'Auth-Token' => user.authentication_token }
+
+        expect(response).to have_http_status(:success)
+    end
   end
 end
